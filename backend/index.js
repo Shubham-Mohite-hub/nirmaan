e3cc706c0006ee238f4da1bd89c3b68b1fe0b51a
Hrushikesh <hrushikeshsuryawanshi832@gmail.com>
2025-01-30 12:55:30 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoute from "./routes/user.route.js";
+
 import messageRoute from "./routes/message.route.js";
 import path from "path";
 import cookieParser from "cookie-parser";
